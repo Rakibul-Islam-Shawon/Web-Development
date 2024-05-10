8087e8a6399cd2a5a5d15c131b93b8446147a001
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-10 13:11:56 +0200

    Arithmatic operator added

--- tut56/index.js
+++ tut56/index.js
@@ -4,6 +4,14 @@ console.log("Hello I am conditional tutorial");
 let age = 35;
 let grace = 20;
 
+//arithmatic operator
+
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+
 if((age - grace) > 18){
     console.log("You can drive.");
 }
