5f67d2c0e76a6d286e5df1aab8185f85404e8838
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-10 13:16:23 +0200

    increment and decrement operator added

--- tut56/index.js
+++ tut56/index.js
@@ -4,13 +4,23 @@ console.log("Hello I am conditional tutorial");
 let age = 35;
 let grace = 20;
 
+//increment assignment operator
+
+// age += grace;
+// console.log(age);
+
+//decrement assignment operator
+
+age -= grace;
+console.log(age);
+
 //arithmatic operator
 
-console.log(age + grace);
-console.log(age - grace);
-console.log(age * grace);
-console.log(age / grace);
-console.log(age ** grace);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
 
 if((age - grace) > 18){
     console.log("You can drive.");
