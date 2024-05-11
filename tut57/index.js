c798a4e3aaeb1815dff30767fc75294e2e39c251
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-11 12:56:59 +0200

    For ... in Loop added

--- tut57/index.js
+++ tut57/index.js
@@ -3,6 +3,16 @@ console.log("I am a loop tutorial");
 
 let a = 1;
 
-for( let i = 0 ; i< 100; i++){
-    console.log(a + i);
+// for( let i = 0 ; i< 100; i++){
+//     console.log(a + i);
+// }
+
+let obj = {
+    name: "Harry",
+    role: "Programmer",
+    company: "CodeWithRakib"
+}
+
+for( const key in obj){
+    console.log(key, obj[key]);
 }
