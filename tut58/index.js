050901f7d453a110adbdced91807df856cb1da8b
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-14 19:47:17 +0200

    JavaScript Functions understanding

---
+++ tut58/index.js
@@ -0,0 +1,10 @@
+function greetings (name){
+
+    console.log("Hi, This is " + name + " and I am an engineer.");
+    console.log("Hi, This is " + name + " and I am an doctor.");
+    console.log("Hi, This is " + name + " and I am an Pilot.");
+    console.log("Hi, This is " + name + " and I am an Plumber.");
+    console.log("Hi, This is " + name + " and I am an Electrician.");
+}
+
+greetings("Rakib");
