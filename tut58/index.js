35bc2cf57db42ee8bf689cfefa8bc23d6a6ff3d3
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-14 19:49:55 +0200

    Summation using function

--- tut58/index.js
+++ tut58/index.js
@@ -1,10 +1,18 @@
-function greetings (name){
+// function greetings (name){
 
-    console.log("Hi, This is " + name + " and I am an engineer.");
-    console.log("Hi, This is " + name + " and I am an doctor.");
-    console.log("Hi, This is " + name + " and I am an Pilot.");
-    console.log("Hi, This is " + name + " and I am an Plumber.");
-    console.log("Hi, This is " + name + " and I am an Electrician.");
+//     console.log("Hi, This is " + name + " and I am an engineer.");
+//     console.log("Hi, This is " + name + " and I am an doctor.");
+//     console.log("Hi, This is " + name + " and I am an Pilot.");
+//     console.log("Hi, This is " + name + " and I am an Plumber.");
+//     console.log("Hi, This is " + name + " and I am an Electrician.");
+// }
+
+// greetings("Rakib");
+
+// greetings("Samir");
+
+function sum (a, b){
+    console.log(a+b);
 }
 
-greetings("Rakib");
+sum(10,20);
