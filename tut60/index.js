888fb60d1a952c0e89e55697dd805f08b61bccd6
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-14 21:17:31 +0200

    different string representation

--- tut60/index.js
+++ tut60/index.js
@@ -1,9 +1,16 @@
 
-console.log("This is strings tutorial");
-let a = "Rakib";
-console.log(a);
-console.log(a[0]);
-console.log(a[1]);
-console.log(a[2]);
-console.log(a[3]);
-console.log(a[4]);
+// console.log("This is strings tutorial");
+// let a = "Rakib";
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+let real_name = "Rakib";
+
+let friend_name = "Samir";
+
+console.log("His name is " + real_name + " and his friend name is "+friend_name);
+console.log(`His name is ${real_name} and his friend name is ${friend_name}`);
