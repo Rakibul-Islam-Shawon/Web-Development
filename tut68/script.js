256ceb6936efc112a85b7c29f699f3afa01e8487
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-15 12:03:20 +0200

    Ids, Classes selecting process in JS

---
+++ tut68/script.js
@@ -0,0 +1,9 @@
+console.log("Hello dom");
+
+// let boxes = document.getElementsByClassName("box");
+
+// console.log(boxes);
+
+// boxes[2].style.backgroundColor = "red";
+
+document.getElementById("redbox").style.backgroundColor = "blue";
