c346c8f2e7a0a605fdd008d87ec8826269896579
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-17 23:11:15 +0200

    this keyword used in method

--- JS_Object/script.js
+++ JS_Object/script.js
@@ -1,18 +1,31 @@
-let car = {
-    name: "BMW",
-    year: "2004",
-    color: "Black",
+// const car = {
+//     name: "BMW",
+//     year: "2004",
+//     color: "Black",
 
-    start: function(){
-        console.log("Car has started.");
-    },
+//     start: function(){
+//         console.log("Car has started.");
+//     },
 
-    drive: function(){
-        console.log("Car has drived.");
+//     drive: function(){
+//         console.log("Car has drived.");
+//     }
+// }
+
+// console.log(car.name);
+// console.log(car.color);
+// car.drive();
+// car.start();
+
+
+const person = {
+    firstName : "Rakibul",
+    lastName  : "Islam",
+    id        : 55455,
+    fullName  : function(){
+        return this.firstName + " " + this.lastName;
     }
+
 }
 
-console.log(car.name);
-console.log(car.color);
-car.drive();
-car.start();
+console.log(person.fullName());
