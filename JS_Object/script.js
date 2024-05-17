8f27bb26fa178ab8cfcc0d0f62e7b03143aa10ff
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-17 22:27:17 +0200

    Method added in JS object

--- JS_Object/script.js
+++ JS_Object/script.js
@@ -1,8 +1,18 @@
 let car = {
     name: "BMW",
     year: "2004",
-    color: "Black"
+    color: "Black",
+
+    start: function(){
+        console.log("Car has started.");
+    },
+
+    drive: function(){
+        console.log("Car has drived.");
+    }
 }
 
 console.log(car.name);
 console.log(car.color);
+car.drive();
+car.start();
