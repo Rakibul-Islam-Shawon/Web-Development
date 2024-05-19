170b99961671d037f2e883e152ee41640a0091c3
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-19 15:24:02 +0200

    Understanding Object accessors

--- JS_Object_Display/script.js
+++ JS_Object_Display/script.js
@@ -1,25 +1,40 @@
-const person = {
-    name: "Rakib",
-    age : 27,
-    city : "Dhaka",
-    today: new Date()
-}
-
-// document.getElementById("demo").innerHTML = person.name + " is "
-//  + person.age + " and lives in "+person.city;
-
-// let text = "";
-
-// for(let x in person){
-//     text = text + person[x] + " ";
+// const person = {
+//     name: "Rakib",
+//     age : 27,
+//     city : "Dhaka",
+//     today: new Date()
 // }
-// // console.log(text);
 
-// document.getElementById("demo").innerHTML = text;
+// // document.getElementById("demo").innerHTML = person.name + " is "
+// //  + person.age + " and lives in "+person.city;
 
-let myString = JSON.stringify(person);
-document.getElementById("demo").innerHTML = myString;
+// // let text = "";
 
-console.log(myString);
+// // for(let x in person){
+// //     text = text + person[x] + " ";
+// // }
+// // // console.log(text);
+
+// // document.getElementById("demo").innerHTML = text;
+
+// let myString = JSON.stringify(person);
+// document.getElementById("demo").innerHTML = myString;
+
+// console.log(myString);
 
 
+
+//Getter and Setter
+
+const person = {
+    firstName: "Rakib",
+    lastName: "Hasan",
+    language: "bn",
+
+    get lang(){
+        return this.language;
+    }
+};
+
+
+console.log(person.lang);
