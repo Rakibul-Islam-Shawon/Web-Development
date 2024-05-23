756eb3c5325b94ad09e534aeb8305887af6e0e6d
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-23 22:28:05 +0200

    Double click event listener added

--- tut74/script.js
+++ tut74/script.js
@@ -5,3 +5,8 @@ button.addEventListener("click", ()=>{
 
     document.querySelector(".box").innerHTML = "Your clicked working.";
 })
+
+button.addEventListener("dblclick", ()=>{
+
+    document.querySelector(".box").innerHTML = "Your double clicked working.";
+})
