f614e57a25742f4977e56cb1a9fbcf0ee8f5d0ee
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-26 23:43:58 +0200

    JS callbacks

---
+++ tut75/script.js
@@ -0,0 +1,13 @@
+console.log("Rakib is becomming programmer");
+console.log("Rifat is a accountant");
+
+
+setTimeout(()=>{
+    console.log("I am inside settimeout");
+}, 2000);
+
+setTimeout(()=>{
+    console.log("I am inside settimeout 2");
+},1000);
+
+console.log("The END");
