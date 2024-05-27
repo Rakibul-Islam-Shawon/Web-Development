1983ccaaccbd30f04dda8a0710c78ca47afcd69e
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-27 11:54:55 +0200

    callback function understanding

--- tut75/script.js
+++ tut75/script.js
@@ -12,3 +12,14 @@ setTimeout(()=>{
 
 console.log("The END");
 console.log("The END");
+
+const callback = (arg)=>{
+    console.log(arg);
+}
+
+const loadScript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.onload = callback("Harry");
+    document.head.append(sc);
+}
