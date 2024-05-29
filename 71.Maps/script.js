3de19c171bc7c1bb6db51cdb925723cde5e0df93
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-29 14:48:45 +0200

    Function Hoisting

--- 71.Maps/script.js
+++ 71.Maps/script.js
@@ -1,10 +1,39 @@
-const fruits = new Map();
+// const fruits = new Map();
 
 
-fruits.set(30, 400);
-fruits.set("bananas", 300);
-fruits.set("oranges", 100);
+// fruits.set(30, 400);
+// fruits.set("bananas", 300);
+// fruits.set("oranges", 100);
 
-console.log(fruits);
+// console.log(fruits);
 
-console.log(fruits.get("bananas"));
+// console.log(fruits.get("bananas"));
+
+
+// Create Objects
+// const apples = {name: 'Apples'};
+// const bananas = {name: 'Bananas'};
+// const oranges = {name: 'Oranges'};
+
+// // Create a Map
+// const fruits = new Map();
+
+// // Add new Elements to the Map
+// fruits.set(apples, 500);
+// fruits.set(bananas, 300);
+// fruits.set(oranges, 200);
+
+// console.log(fruits.get(apples));
+
+
+//Function hoisting
+
+
+let a = sum(2,3);
+
+function sum(a,b){
+    return a+b;
+}
+
+
+console.log(a);
