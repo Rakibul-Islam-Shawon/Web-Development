539f74bb542bb78b6ee273b3b31cb15b9298f12b
Md Rakibul Islam <shawonnstu@gmail.com>
2024-05-31 13:04:21 +0200

    Modulas by Function

--- JS Function/script.js
+++ JS Function/script.js
@@ -4,17 +4,25 @@
 
 // console.log(add(2,5));
 
-//Argument object
-x = findMax(1, 123, 500, 115, 44, 88);
+// //Argument object
+// x = findMax(1, 123, 500, 115, 44, 88);
 
-function findMax() {
-  let max = -Infinity;
-  for (let i = 0; i < arguments.length; i++) {
-    if (arguments[i] > max) {
-      max = arguments[i];
-    }
-  }
-  return max;
+// function findMax() {
+//   let max = -Infinity;
+//   for (let i = 0; i < arguments.length; i++) {
+//     if (arguments[i] > max) {
+//       max = arguments[i];
+//     }
+//   }
+//   return max;
+// }
+
+// console.log(x);
+
+
+
+function Modulas(x, y){
+  return (x%y);
 }
 
-console.log(x);
+console.log(Modulas(3,5));
