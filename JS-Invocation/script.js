ce64d0192d741799d68f27443fd844350066f381
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-01 14:06:27 +0200

    Small change in code

--- JS-Invocation/script.js
+++ JS-Invocation/script.js
@@ -7,9 +7,11 @@
 function myFunction(arg1, arg2){
     this.firstName = arg1;
     this.lastName = arg2;
+    this.fullName = this.firstName +" "+ this.lastName;
 }
 
 const myObj = new myFunction("Rakib", "Khan");
 
 console.log(myObj.firstName);
 console.log(myObj.lastName);
+console.log(myObj.fullName);
