b8ae5f6e9edf5aad0afafe2c5b427c691a0ae3ad
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-06 15:11:23 +0200

    Syntax error thrown

--- tut79/script.js
+++ tut79/script.js
@@ -1,6 +1,10 @@
 let a = prompt("Enter first number: ");
 let b = prompt("Enter second Number: ");
 
-let sum = a + b;
+if(isNaN(a) || isNaN(b)){
+    throw SyntaxError("Sorry you made a mistake.");
+}
+
+let sum = parseInt(a) + parseInt(b);
 
 console.log("The sum is : ", sum);
