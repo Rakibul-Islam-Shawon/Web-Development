0886a5b1dc74ad90a714230e3c0b8ac0df60ad17
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-07 12:06:24 +0200

    JS object constructor

--- tut80/script.js
+++ tut80/script.js
@@ -10,16 +10,32 @@
 // document.getElementById("demo").innerHTML =
 // person.firstName + " is " + person.age + " years old.";
 
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//   }
+  
+//   // Create a Person object
+//   const myFather = new Person("John", "Doe", 50, "blue");
+  
+//   // Display age
+//   document.getElementById("demo").innerHTML =
+//   "My father is " + myFather.age + "."; 
+
 function Person(first, last, age, eye) {
     this.firstName = first;
     this.lastName = last;
     this.age = age;
     this.eyeColor = eye;
+    this.nationality = "English";
   }
   
-  // Create a Person object
+  // Create 2 Person objects
   const myFather = new Person("John", "Doe", 50, "blue");
+  const myMother = new Person("Sally", "Rally", 48, "green");
   
-  // Display age
+  // Display nationality
   document.getElementById("demo").innerHTML =
-  "My father is " + myFather.age + "."; 
+  "My father is " + myFather.nationality + ". My mother is " + myMother.nationality;
