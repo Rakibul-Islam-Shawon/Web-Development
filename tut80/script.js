74b6d4b3f92e503d9338d37a8cfaa5dd570ad1f1
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-07 12:04:52 +0200

    JS object constractor

--- tut80/script.js
+++ tut80/script.js
@@ -1,11 +1,25 @@
-const person = {};
+// const person = {};
 
-// Add Properties
-person.firstName = "John";
-person.lastName = "Doe";
-person.age = 50;
-person.eyeColor = "blue"; 
+// // Add Properties
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue"; 
 
-// Display Data from Object
-document.getElementById("demo").innerHTML =
-person.firstName + " is " + person.age + " years old.";
+// // Display Data from Object
+// document.getElementById("demo").innerHTML =
+// person.firstName + " is " + person.age + " years old.";
+
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+  
+  // Create a Person object
+  const myFather = new Person("John", "Doe", 50, "blue");
+  
+  // Display age
+  document.getElementById("demo").innerHTML =
+  "My father is " + myFather.age + "."; 
