deb33767cba3240e8296dc2485c4023486fbe3e2
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-13 14:35:24 +0200

    Changing by targeting id/class

--- JS_DOM_Document/script.js
+++ JS_DOM_Document/script.js
@@ -2,4 +2,6 @@ console.log(document.getElementsByTagName("a"));
 console.log(document.getElementsByClassName("anchor"));
 console.log(document.getElementById("demo"));
 
+
+console.log(document.getElementById("anchor").innerHTML = "New thisgs");
 // document.getElementsByTagName()
