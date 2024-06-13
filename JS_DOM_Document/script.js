4560d0e32de50fb1cddb24e19ac313fcdb839e39
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-13 14:19:19 +0200

    Finding HTML Elements

---
+++ JS_DOM_Document/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByTagName("a"));
+console.log(document.getElementsByClassName("anchor"));
+console.log(document.getElementById("demo"));
+
+// document.getElementsByTagName()
