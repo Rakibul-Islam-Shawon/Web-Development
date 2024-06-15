9bb491db3e5ec70cfb07eb442d4adacb3ae469fc
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-15 14:16:03 +0200

    Query Seclector used

--- JS_DOM_Document/script.js
+++ JS_DOM_Document/script.js
@@ -2,6 +2,7 @@ console.log(document.getElementsByTagName("a"));
 console.log(document.getElementsByClassName("anchor"));
 console.log(document.getElementById("demo"));
 
+document.querySelectorAll(.test);
 
 console.log(document.getElementById("anchor").innerHTML = "New thisgs");
 // document.getElementsByTagName()
