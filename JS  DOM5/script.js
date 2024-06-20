c2ca63fb996d66d4c52e5b652a4e407360970edb
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-20 13:20:58 +0200

    Dynamic Html content

--- JS  DOM5/script.js
+++ JS  DOM5/script.js
@@ -1,7 +1,12 @@
-const element = document.getElementById("myImage");
-element.src =  "landscape.jpg";
+// const element = document.getElementById("myImage");
+// element.src =  "landscape.jpg";
 
-const element1 = document.getElementById("id01");
+// const element1 = document.getElementById("id01");
 
-element.innerHTML = "Hi";
-element.innerHTML = "hello";
+// element.innerHTML = "Hi";
+// element.innerHTML = "hello";
+
+
+const element = document.getElementById("demo");
+
+element.innerHTML = "Date: " + Date();
