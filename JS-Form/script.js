6b66f1fce29e910fc9f640e140fedce1c15f8fc6
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-22 00:06:01 +0200

    JS validation within condition

--- JS-Form/script.js
+++ JS-Form/script.js
@@ -1,12 +1,26 @@
 function validateForm(){
+    // const form = document.forms['myForm'];
+    // const value = form ['fname'].value;
+
+    // if(value == ""){
+    //     const text = "You have to write your name!";
+    //     document.getElementById("demo").innerHTML = text;
+
+    //     return false;
+    // }
+
     const form = document.forms['myForm'];
     const value = form ['fname'].value;
-
-    if(value == ""){
-        const text = "You have to write your name!";
-        document.getElementById("demo").innerHTML = text;
-
-        return false;
+    let text;
+    if( isNaN(value) || value < 1 || value > 10){
+        text = "Input not valid!";
+        
     }
+    else{
+        text = "input is ok";
+    }
+    document.getElementById("demo").innerHTML = text;
 
+
+    return false;
 }
