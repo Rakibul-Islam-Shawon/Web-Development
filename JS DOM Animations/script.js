7cfa9c26b2165c6b032c841a10f424631376d460
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-23 23:11:45 +0200

    Now the animation working

--- JS DOM Animations/script.js
+++ JS DOM Animations/script.js
@@ -4,5 +4,14 @@ function myMove() {
     let pos = 0;
     clearInterval(id);
     id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+          clearInterval(id);
+        } else {
+          pos++; 
+          elem.style.top = pos + "px"; 
+          elem.style.left = pos + "px"; 
+        }
+      }
     
   }
