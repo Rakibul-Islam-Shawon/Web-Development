6e7f4249d3608aeba3e825ef5b5c617cc0cb88b2
Md Rakibul Islam <shawonnstu@gmail.com>
2024-06-23 23:11:15 +0200

    JS code added to active Animation

---
+++ JS DOM Animations/script.js
@@ -0,0 +1,8 @@
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");   
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    
+  }
