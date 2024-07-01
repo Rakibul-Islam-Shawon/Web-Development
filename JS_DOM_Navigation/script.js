f5992d39b4a72cc5b7d13cc5d85ee9034ec0c224
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-01 23:57:23 +0200

    change in both file

--- JS_DOM_Navigation/script.js
+++ JS_DOM_Navigation/script.js
@@ -2,4 +2,6 @@
 
 // document.getElementById("id02").innerHTML = document.getElementById("id01").firstChild.nodeValue;
 
-document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[0].nodeValue;
+// document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[0].nodeValue;
+
+document.getElementById("demo").innerHTML = document.body.innerHTML;
