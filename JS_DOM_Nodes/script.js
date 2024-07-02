60386fde5bfaf6b648d56ad991c6729e32b8a539
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-02 21:13:49 +0200

    text shown in browser using appendChild function

---
+++ JS_DOM_Nodes/script.js
@@ -0,0 +1,7 @@
+const para = document.createElement("p");
+const node = document.createTextNode("Hello world");
+
+para.appendChild(node);
+
+const element = document.getElementById("div1");
+element.appendChild(para);
