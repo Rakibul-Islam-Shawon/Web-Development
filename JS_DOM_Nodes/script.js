394f6560eb28457fc555835aefcc610a14f92f35
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-02 21:34:46 +0200

    Removing process of para using JS

--- JS_DOM_Nodes/script.js
+++ JS_DOM_Nodes/script.js
@@ -1,10 +1,15 @@
-const para = document.createElement("p");
-const node = document.createTextNode("Hello world");
+// const para = document.createElement("p");
+// const node = document.createTextNode("Hello world");
 
-para.appendChild(node);
+// para.appendChild(node);
 
-const element = document.getElementById("div1");
-element.appendChild(para);
+// const element = document.getElementById("div1");
+// element.appendChild(para);
 
-const p2 = document.getElementById("p2");
-element.insertBefore(para, p2);
+// const p2 = document.getElementById("p2");
+// element.insertBefore(para, p2);
+
+
+const p3 = document.getElementById("p2");
+
+p3.remove();
