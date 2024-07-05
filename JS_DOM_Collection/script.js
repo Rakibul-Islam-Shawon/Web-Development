7c4de9e96f4ba5b1a18c2bdcc36c637a037e18af
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-05 23:18:03 +0200

    Button added for collection use

--- JS_DOM_Collection/script.js
+++ JS_DOM_Collection/script.js
@@ -1,4 +1,12 @@
-const myCollection = document.getElementsByTagName("p");
+// const myCollection = document.getElementsByTagName("p");
 
-document.getElementById("demo").innerHTML = 
-"The innerHTML of the second paragraph is: " + myCollection[1].innerHTML;
+// document.getElementById("demo").innerHTML = 
+// "The innerHTML of the second paragraph is: " + myCollection[1].innerHTML;
+
+
+function myFunction() {
+    const myCollection = document.getElementsByTagName("p");
+    for (let i = 0; i < myCollection.length; i++) {
+      myCollection[i].style.color = "red";
+    }
+  }
