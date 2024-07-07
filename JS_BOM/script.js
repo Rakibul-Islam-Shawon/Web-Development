e96babc00320cbbc2aba6a2e6e29a816fc87ef72
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-07 23:35:41 +0200

    Screen color added

--- JS_BOM/script.js
+++ JS_BOM/script.js
@@ -7,5 +7,12 @@
 // "Screen width is " + screen.width;
 
 
+// document.getElementById("demo").innerHTML = 
+// "Screen height is " + screen.height;
+
+// document.getElementById("demo").innerHTML = 
+// "Available screen height is " + screen.availHeight;
+
+
 document.getElementById("demo").innerHTML = 
-"Screen height is " + screen.height;
+"Screen color depth is " + screen.colorDepth;
