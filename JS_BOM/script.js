fe3eaa3ff8ad86586d76e5be540610b41c66078e
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-08 22:10:11 +0200

    Uncommented some code for testing

--- JS_BOM/script.js
+++ JS_BOM/script.js
@@ -1,17 +1,17 @@
-// document.getElementById("demo").innerHTML =
-// "Browser inner window width: " + window.innerWidth + "px<br>" +
-// "Browser inner window height: " + window.innerHeight + "px";
+document.getElementById("demo").innerHTML =
+"Browser inner window width: " + window.innerWidth + "px<br>" +
+"Browser inner window height: " + window.innerHeight + "px";
 
 
 // document.getElementById("demo").innerHTML = 
 // "Screen width is " + screen.width;
 
 
-document.getElementById("demo").innerHTML = 
-"Screen height is " + screen.height;
+// document.getElementById("demo").innerHTML = 
+// "Screen height is " + screen.height;
 
-document.getElementById("demo").innerHTML = 
-"Available screen height is " + screen.availHeight;
+// document.getElementById("demo").innerHTML = 
+// "Available screen height is " + screen.availHeight;
 
 
 // document.getElementById("demo").innerHTML = 
