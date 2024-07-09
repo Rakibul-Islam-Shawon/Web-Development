a81e6e8f556d486ced301c70eb36cc2819f15fac
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-09 23:35:00 +0200

    Changed the code

--- JS_Type_Conversion/script.js
+++ JS_Type_Conversion/script.js
@@ -1,3 +1,11 @@
-let y = "John";
-let x = + y;
-document.getElementById("demo").innerHTML = typeof y + "<br>" + typeof x;
+// let y = "John";
+// let x = + y;
+// document.getElementById("demo").innerHTML = typeof y + "<br>" + typeof x;
+
+
+
+let x = 123;
+document.getElementById("demo").innerHTML =
+  x.toString() + "<br>" +
+   (123).toString() + "<br>" +
+   (100 + 23).toString();
