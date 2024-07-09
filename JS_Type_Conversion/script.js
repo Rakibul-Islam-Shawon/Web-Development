2ca239822d7fee4b6b712cc85cf1f183100ead91
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-09 23:36:41 +0200

    Slight change for better understanding

--- JS_Type_Conversion/script.js
+++ JS_Type_Conversion/script.js
@@ -4,8 +4,24 @@
 
 
 
-let x = 123;
+// let x = 123;
+// document.getElementById("demo").innerHTML =
+//   x.toString() + "<br>" +
+//    (123).toString() + "<br>" +
+//    (100 + 23).toString();
+
+
+// document.getElementById("demo").innerHTML =
+// (5 + null) + "<br>"  +
+// ("5" + null) + "<br>" +
+// ("5" + 2) + "<br>" +
+// ("5" - 2) + "<br>" +
+// ("5" * "2") + "<br>" +
+// ("5" / "2") + "<br>"
+
+
+let x = undefined;
 document.getElementById("demo").innerHTML =
-  x.toString() + "<br>" +
-   (123).toString() + "<br>" +
-   (100 + 23).toString();
+"Number : " + Number(x) + "<br>" +
+"String : " + String(x) + "<br>" +
+"Boolean: " + Boolean(x);
