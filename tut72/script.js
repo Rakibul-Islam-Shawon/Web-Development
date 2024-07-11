2f3ca2ee7dfbe4229095baec4ac802f3e5784491
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-11 21:04:51 +0200

    Random color generate successfully

--- tut72/script.js
+++ tut72/script.js
@@ -5,9 +5,9 @@ let boxes = document.getElementsByClassName("box");
 
 
 function getRandomColor(){
-    let val1 = Math.random();
-    let val2 = Math.random();
-    let val3 = Math.random();
+    let val1 =  Math.ceil (0 + Math.random()* 255);
+    let val2 = Math.ceil (0 + Math.random()* 255);
+    let val3 = Math.ceil (0 + Math.random()* 255);
 
 
     return `rgb(${val1}, ${val2}, ${val3})`;
