65ebb1ef222fa86cd5bd83a0f3da1f1484dcd180
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-13 23:30:12 +0200

    Discussion about the fuction calling

--- Function Call/script.js
+++ Function Call/script.js
@@ -1,8 +1,25 @@
-const myObject = {
-    firstName:"John",
-    lastName: "Doe",
+// const myObject = {
+//     firstName:"John",
+//     lastName: "Doe",
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+//   document.getElementById("demo").innerHTML = myObject.fullName();
+
+
+
+const person = {
     fullName: function() {
       return this.firstName + " " + this.lastName;
     }
   }
-  document.getElementById("demo").innerHTML = myObject.fullName();
+  const person1 = {
+    firstName:"John",
+    lastName: "Doe"
+  }
+  const person2 = {
+    firstName:"Mary",
+    lastName: "Doe"
+  }
+  document.getElementById("demo").innerHTML = person.fullName.call(person1); 
