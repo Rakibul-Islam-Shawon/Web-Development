2f45812df0bd38eec214a36620c567266f278df8
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-16 23:56:11 +0200

    CHange the code for better understanding

--- JS_FOR_Of/script.js
+++ JS_FOR_Of/script.js
@@ -1,7 +1,17 @@
-const cars = ["BMW", "Volvo", "Mini"];
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// let text = "";
+// for (let x of cars) {
+//   text += x + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
+
+
+let language = "JavaScript";
 
 let text = "";
-for (let x of cars) {
+for (let x of language) {
   text += x + "<br>";
 }
 
