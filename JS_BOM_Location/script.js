2780b6a46d4a4dd6200ce84e07384ad9f91b4b43
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-18 23:49:05 +0200

    Location updated

--- JS_BOM_Location/script.js
+++ JS_BOM_Location/script.js
@@ -3,5 +3,8 @@
 
 
 
-document.getElementById("demo").innerHTML =
-"Page path is: " + window.location.pathname;
+// document.getElementById("demo").innerHTML =
+// "Page path is: " + window.location.pathname;
+
+document.getElementById("demo").innerHTML = 
+"The URL port number of the current page is: " + window.location.port;
