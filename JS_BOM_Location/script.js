27744025eecebf820e6601b77ba45fca1fecd896
Md Rakibul Islam <shawonnstu@gmail.com>
2024-07-18 23:44:18 +0200

    JS Path showing:

--- JS_BOM_Location/script.js
+++ JS_BOM_Location/script.js
@@ -1,2 +1,7 @@
-document.getElementById("demo").innerHTML = 
-"The full URL of this page is:<br>" + window.location.href;
+// document.getElementById("demo").innerHTML = 
+// "The full URL of this page is:<br>" + window.location.href;
+
+
+
+document.getElementById("demo").innerHTML =
+"Page path is: " + window.location.pathname;
